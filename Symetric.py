dd1bca9fcd33db440c59ae6407cec3e0a3196f00
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-10 13:15:00 +0530

    Symetric using xor operator

---
+++ Symetric.py
@@ -0,0 +1,9 @@
+#Find Symetric in Set
+Set1={20,33,40,30,10}
+Set2={2,33,1,3,10}
+
+#Symetric through (^)
+Set3=Set1^Set2
+print("Set1 :",Set1)
+print("Set2 :",Set2)
+print("Symmetric of Set :",Set3)
