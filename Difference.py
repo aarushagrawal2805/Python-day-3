f8f6da17b3498d3529d8d1837a60780bd6720038
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-10 13:01:10 +0530

    Difference Between Set

---
+++ Difference.py
@@ -0,0 +1,11 @@
+#Difference of Sets
+Set1={10,33,44,55,77,"Aryan"}
+Set2={1,3,44,55,77,"Amit"}
+
+#First Way .differnce
+Set3=Set1.difference(Set2)
+print("Difference Set ('.difference'):",Set3)
+
+#Difference Second Way -
+Set4=Set1-Set2
+print("Difference Set ('-') :",Set4)
