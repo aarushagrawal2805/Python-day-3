ce4cccb6ca993b6e27e56420c937c6e8b4e7d09c
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-10 15:51:16 +0530

    Basic Syntax

--- Dictionary/Basic.py
+++ Dictionary/Basic.py
@@ -1,1 +1,14 @@
-print("heeloo")
+my_dict={"Name":"Amit","Age":20,"Address":"Indore"}
+print("Initial Dict :",my_dict)
+#Add New Key-Value Pair
+
+my_dict["Profeesion"]="Doctor"
+
+print("After Add new item :",my_dict)
+
+#Modify Value
+my_dict["Age"]=30
+print("After Modify Age :",my_dict)
+
+#Access Key
+print("Address :",my_dict["Address"])
