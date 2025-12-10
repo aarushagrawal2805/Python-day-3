625bb287ce3f577f1b3628d87f6af47bf8861e19
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-10 12:43:15 +0530

    Basic Syntax

---
+++ Basic.py
@@ -0,0 +1,15 @@
+# Print A Set
+Fruits = {"Apple", "Orange", "Mango", "Banana"}
+print("Initial Set :", Fruits)
+
+# Add Element in set
+Fruits.add("Grapes")
+print("Set after add Element :", Fruits)
+
+# Remove Element from set
+Fruits.remove("Banana")
+print("Set after Remove Element 'Banana' :", Fruits)
+
+# Discard Element From Set
+Fruits.discard("Mango")
+print("Set after Discard Element 'Mango' :", Fruits)
