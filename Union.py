88edb52d2f51633f04212139ee0484a80d08f01c
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-10 12:49:56 +0530

    Union Set

---
+++ Union.py
@@ -0,0 +1,12 @@
+#Union of Set
+Set1 = {10, 20, 30, 40, 50}
+Set2 = {30, 40, 50, 60, 70}
+
+#The First Way to union is .Union
+Set3=Set1.union(Set2)
+print("Union Set With (.Union) :",Set3)
+
+
+#The Second Way to Union is |
+Set4=Set1|Set2
+print("Union Set ('|'):",Set4)
